3be55c693fb00f22226187d96c74d2dbda564334
Samuel Baird <sam@samuelbaird.com>
2023-06-15 15:13:47 -0300

    Wrote pseudo code and added wireframe image

---
+++ main.js
@@ -0,0 +1,42 @@
+/*----- constants -----*/
+
+
+/*----- state variables -----*/
+
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Pseudocode!
+- define variables
+- initialize home screen
+- button with event listener to start the Game
+- animate player, ground, obstacles, holes, and finish line using the DOM
+- create gravity logic
+- create object collision logic
+- update win/loss/playing status based on contact/noncontact with holes/finish line
+- tie player movements to keys
+- update on screen messages based on game state
+- create hidden play again button
+
+EXTRAS
+- Scroll to next page of level (no canvas, only DOM!)
+- character jump/walk/direction animations
+- death/win animations
