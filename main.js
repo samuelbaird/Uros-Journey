9bfbd8bd91224373fda47e695f8f81bc722cf286
Samuel Baird <sam@samuelbaird.com>
2023-06-19 22:28:39 -0300

    troubleshooting movement

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 /*----- constants -----*/
 let uro = document.getElementById("Uro");
 let uroX = 0;
-let uroY = 0;
-let accelerationY = 700;
-let speedX = 10;
-let speedY = 20;
+let uroY = 20;
+let accelerationY = 350;
+let speedX = 5;
+let speedY = 0;
 let gameWindow = document.querySelector("main");
 let gameWidth = gameWindow.innerWidth;
 let gameHeight = gameWindow.innerHeight;
@@ -68,12 +68,12 @@ function render(timestamp) {
         uro.style.transform = `translate(${uroY - 10}px)`
     }
   } else {
-    uro.style.transform = `translate(${uroX}px, ${uroY}px)`;
+    uro.style.transform = `translate(${uroX}px, ${uroY}px) ${lastScaleX}`;
   }
   obstacles.forEach((obstacle) => {
     if (isCollide(uro, obstacle)) {
       speedY = 0;
-      uroY = obstacle.getBoundingClientRect().top - uro.offsetHeight - 5;
+      // uroY = obstacle.getBoundingClientRect().top - uro.offsetHeight - 5;
     }
   });
 
@@ -89,29 +89,17 @@ function isCollide(uro, b) {
   let uroRect = uro.getBoundingClientRect();
   let bRect = b.getBoundingClientRect();
   return !(
-    uroRect.top + uroRect.offsetHeight < bRect.top ||
-    uroRect.top > bRect.top + bRect.offsetHeight ||
-    uroRect.left + uroRect.offsetWidth < bRect.left ||
-    uroRect.left > bRect.left + bRect.offsetWidth
+    // uroRect.top + uroRect.offsetHeight < bRect.top ||
+    // uroRect.top > bRect.top + bRect.offsetHeight ||
+    // uroRect.left + uroRect.offsetWidth < bRect.left ||
+    // uroRect.left > bRect.left + bRect.offsetWidth
+
+    ((uroRect.y + uroRect.offsetHeight) < (bRect.y)) ||
+    (uroRect.y > (bRect.y + bRect.offsetHeight)) ||
+    ((uroRect.x + uroRect.width) < bRect.x) ||
+    (uroRect.x > (bRect.x + bRect.width))
   );
 }
 
 let previousTimestamp = 0;
 init();
-
-/* Pseudocode!
-- define variables
-- initialize home screen
-- button with event listener to start the Game
-- animate player, ground, obstacles, holes, and finish line using the DOM
-- create gravity logic
-- create object collision logic
-- update win/loss/playing status based on contact/noncontact with holes/finish line
-- tie player movements to keys
-- update on screen messages based on game state
-- create hidden play again button
-
-EXTRAS
-- Scroll to next page of level (no canvas, only DOM!)
-- character jump/walk/direction animations
-- death/win animations */
