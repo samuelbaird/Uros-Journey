a63969883108a5bc1c27346f553e2d20b978a22c
Samuel Baird <sam@samuelbaird.com>
2023-06-21 18:01:49 -0300

    troubleshooting play again and game reset

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 let uro = document.getElementById("Uro");
-let uroX = 0;
-let uroY = 280;
+let uroX = 10;
+let uroY = 10;
 let accelerationY = 2000;
 let speedX = 5;
 let speedY = 0;
 let gameWindow = document.querySelector("main");
-let gameWidth = gameWindow.innerWidth;
-let gameHeight = gameWindow.innerHeight;
+let gameWidth = 960;
+let gameHeight = 540;
 let obstacles = document.querySelectorAll(".Ground");
 let traps = document.querySelectorAll(".traps");
+let home = document.getElementById("home");
 let arrowInput;
 let moving = false;
 let lastScaleX;
@@ -61,11 +62,16 @@ function render(timestamp) {
   const dt = timestamp - previousTimestamp;
   previousTimestamp = timestamp;
   gravity(dt / 1000);
+  if (uroY + uro.offsetHeight > gameHeight) {
+    uroY = gameHeight - uro.offsetHeight;
+    speedY = 0;
+    collisionDetected = true;
+  }
   if (moving) {
-    if (arrowInput === "left") {
+    if (arrowInput === "left" && uroX > 0) {
       uroX -= speedX;
       uro.style.transform = `translate(${uroX}px, ${uroY}px) ${lastScaleX}`;
-    } else if (arrowInput === "right") {
+    } else if (arrowInput === "right" && uroX < gameWidth - uro.offsetWidth) {
       uroX += speedX;
       uro.style.transform = `translate(${uroX}px, ${uroY}px) ${lastScaleX}`;
     }
@@ -80,6 +86,7 @@ function render(timestamp) {
     uro.style.transform = `translate(${uroX}px, ${uroY}px) ${lastScaleX}`;
   }
   let collisionDetectedTemp = false;
+  collisionDetected = false;
   if (collisionChecking) {
     obstacles.forEach((obstacle) => {
       if (isCollide(uro, obstacle)) {
@@ -94,8 +101,18 @@ function render(timestamp) {
         uroY = trap.getBoundingClientRect().top - uro.offsetHeight - 70;
         collisionDetectedTemp = true;
         gameStatus = -1;
+        return;
       }
     });
+    if (isCollide(uro, home)) {
+      gameStatus = 1;
+      return;
+    }
+  }
+  if (gameStatus === -1) {
+    collisionChecking = false;
+    loseGame();
+    return;
   }
   collisionDetected = collisionDetectedTemp;
   requestAnimationFrame(render);
@@ -116,6 +133,56 @@ function isCollide(uro, b) {
     uroRect.left > bRect.right
   );
   return collisionDetected;
+}
+
+function loseGame() {
+  let loseImage = document.createElement("img");
+  let loseMessage = document.createElement("h1");
+  let playAgain = document.createElement("h3");
+  playAgain.classList.add("grow");
+  loseImage.src = "/assets/loseImage.png";
+  loseImage.style.opacity = "0";
+  loseMessage.style.opacity = "0";
+  playAgain.style.opacity = "0";
+  loseMessage.style.position = "absolute";
+  loseImage.style.position = "absolute";
+  playAgain.style.position = "absolute";
+  loseMessage.style.color = "rgba(155, 0, 0, 0.8)";
+  loseMessage.style.filter = "drop-shadow(0px 0px 10px rgba(225,232,238,255))";
+  loseMessage.style.transform = "translate(50%, -50%)";
+  playAgain.style.transform = "translate(500%, 1300%)";
+  loseImage.style.transition = "opacity 1s ease";
+  loseMessage.style.transition = "opacity 1s ease";
+  playAgain.style.transition = "opacity 1s ease";
+  loseImage.style.objectFit = "cover";
+  loseImage.style.width = "100%";
+  loseImage.style.maxHeight = "100%";
+  loseImage.style.left = "0";
+  loseMessage.innerText = "The Rotbringer found you";
+  playAgain.innerText = "Play Again";
+  gameWindow.appendChild(loseImage);
+  gameWindow.appendChild(loseMessage);
+  gameWindow.appendChild(playAgain);
+  setTimeout(() => {
+    loseImage.style.opacity = "1";
+    loseMessage.style.opacity = "1";
+    playAgain.style.opacity = "1";
+  }, 50);
+  playAgain.addEventListener("click", restartGame);
+
+  function restartGame() {
+    uroY = 10;
+    uroX = 10;
+    speedX = 0;
+    gameStatus = null;
+    uro.style.left = "10px";
+    uro.style.top = "10px";
+    loseImage.remove();
+    loseMessage.remove();
+    playAgain.removeEventListener("click", restartGame);
+    playAgain.remove();
+    init();
+  }
 }
 
 let previousTimestamp = 0;
